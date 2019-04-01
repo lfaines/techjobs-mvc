e1061d063bafc3ebff96fb74b287bba19d796d87
Blessed <codingdreams4ever@gmail.com>
2019-03-31 19:12:08 -0500

    Unit 3 Assignment #2 Completed

--- src/main/java/org/launchcode/controllers/SearchController.java
+++ src/main/java/org/launchcode/controllers/SearchController.java
@@ -4,6 +4,7 @@ import org.launchcode.models.JobData;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.ArrayList;
@@ -23,5 +24,26 @@ public class SearchController {
     }
 
     // TODO #1 - Create handler to process search request and display results
+    @RequestMapping(value = "results", method = RequestMethod.GET)
+    public String processSearchRequest(Model model, @RequestParam String searchType, @RequestParam String searchTerm) {
+        model.addAttribute("columns", ListController.columnChoices);
+        ArrayList<HashMap<String, String>> jobs = JobData.findByValue(searchTerm);
+        //model.addAttribute("nothing", 0);
 
-}
+        model.addAttribute("jobs", jobs);
+
+        //jobs = JobData.findByColumnAndValue(searchType, searchTerm);
+        //return "search";
+
+        if (searchType.equals("all")) {
+            jobs = JobData.findByValue((searchTerm));
+        }
+
+        else {
+            jobs = JobData.findByColumnAndValue(searchType, searchTerm);
+        }
+
+        return "search";
+
+        }
+    }
